9ee21cc740110167d1cc57b34a0a5e18fdc0fbcd
jefferson-norberto2 <jmn@cin.ufpe.br>
2026-09-09 13:33:39 -0300

    FIX: Controller Hz run

--- .docker/run.sh
+++ .docker/run.sh
@@ -2,11 +2,11 @@
 xhost +local:docker
 
 docker run -d -it \
-  --name kortex \
+  --name kortex_1.3.4 \
   --gpus all \
   -e NVIDIA_DRIVER_CAPABILITIES=all \
   -e DISPLAY=$DISPLAY \
   -e QT_X11_NO_MITSHM=1 \
   --mount type=bind,source=/tmp/.X11-unix,target=/tmp/.X11-unix \
   --device /dev/dri:/dev/dri \
-  kortex_jazzy:1.2.2
+  kortex_jazzy:1.3.4
